349bbb65f7cbe708a3b2cfe8e1cc6d2d8bedabef
Francesco Piccinno <stack.box@gmail.com>
2012-12-04 11:19:46 +0100

    Add possibility of batch conversion

--- helpers/scripts/ff-util.py
+++ helpers/scripts/ff-util.py
@@ -38,12 +38,19 @@ if __name__ == "__main__":
                       help="Cat the contents of the follower file")
     parser.add_option("--convert", action="store_true", dest="convert",
                       help="Convert the old version of the file to the new version")
+    parser.add_option("--list", dest="listfile",
+                      help="Do a batch conversion of the files listed in this file")
 
     (options, args) = parser.parse_args()
 
     if options.cat:
         cat(args[0])
     elif options.convert:
-        convert(args[0])
+        if options.listfile:
+            with open(options.listfile) as input:
+                for line in input:
+                    convert(line.strip())
+        else:
+            convert(args[0])
     else:
         parser.print_help()
